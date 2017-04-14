006f0864655e0fa2e29308199446fb8e2d67e2b0
T.Miyake <info@miyake.me>
2017-04-15 01:16:04 +0900

    Initial commit.
    Initial commit.

---
+++ OnedriveSwitch/Model.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+
+namespace OnedriveSwitch
+{
+    public sealed class Model
+    {
+        // Singleton instance.
+        public static Model Instance { get; } = new Model();
+        private Model(){ }
+        
+        public bool CurrentState { get; set; } = true;
+
+        public void GetCurrentStatus()
+        {
+            switch (ReadReg(@"SOFTWARE\Policies\Microsoft\Windows\OneDrive", "DisableFileSyncNGSC"))
+            {
+                case 0:
+                    CurrentState = true;
+                    break;
+                case 1:
+                    CurrentState = false;
+                    break;
+                default:
+                    CurrentState = true;
+                    break;
+            }
+        }
+
+        public void DoEnable()
+        {
+            WriteReg(@"SOFTWARE\Policies\Microsoft\Windows\OneDrive", "DisableFileSyncNGSC",0);
+        }
+
+        public void DoDisable()
+        {
+            WriteReg(@"SOFTWARE\Policies\Microsoft\Windows\OneDrive", "DisableFileSyncNGSC", 1);
+        }
+
+
+        public void WriteReg(string subKey, string keyName, int value)
+        {
+            var key = Registry.LocalMachine.CreateSubKey(subKey);
+            key?.SetValue(keyName, value, RegistryValueKind.DWord);
+            key?.Close();
+        }
+
+        public int ReadReg(string subKey, string keyName)
+        {
+            try
+            {
+                var key = Registry.LocalMachine.OpenSubKey(subKey);
+                return (int?) key?.GetValue(keyName) ?? 0;
+            }
+            catch
+            {
+                return 0;
+            }
+        }
+    }
+}
---
+++ OnedriveSwitch/ViewModel.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Diagnostics;
+using System.Windows.Markup.Localizer;
+using System.Windows.Media;
+
+namespace OnedriveSwitch
+{
+    public class ViewModel:ViewModelBase
+    {
+        private Model Model = Model.Instance;
+
+        public ICommand Enable { get; }
+        public ICommand Disable { get; }
+
+        public ViewModel()
+        {
+            Enable = new RelayCommand(()=>
+            {
+                Model.DoEnable();
+                CurrentState = true;
+                ChangeButton();
+
+                if (MessageBox.Show("有効にしました。" + Environment.NewLine + "サインアウトしますか？。", "OnedriveSwitch", MessageBoxButton.YesNo, MessageBoxImage.Information) == MessageBoxResult.Yes)
+                    SignOut();
+            });
+
+            Disable = new RelayCommand(() =>
+            {
+                Model.DoDisable();
+                CurrentState = false;
+                ChangeButton();
+
+                if (MessageBox.Show("無効にしました。" + Environment.NewLine + "サインアウトしますか？。", "OnedriveSwitch", MessageBoxButton.YesNo, MessageBoxImage.Information) == MessageBoxResult.Yes)
+                    SignOut();
+            });
+
+            Model.GetCurrentStatus();
+            ChangeButton();
+        }
+
+        public bool CurrentState
+        {
+            get => Model.CurrentState;
+            set
+            {
+                Model.CurrentState = value;
+                OnPropertyChanged("CurrentStateText");
+            }
+        }
+
+        public string CurrentStateText => Model.CurrentState ? "OneDrive：有効" : "OneDrive：無効";
+
+        private bool _enableButton;
+        public bool EnableButton
+        {
+            get => _enableButton;
+            set
+            {
+                _enableButton = value;
+                OnPropertyChanged("EnableButton");
+            }
+        }
+
+        private bool _disableButton;
+        public bool DisableButton
+        {
+            get => _disableButton;
+            set
+            {
+                _disableButton = value;
+                OnPropertyChanged("DisableButton");
+            }
+        }
+
+        public void ChangeButton()
+        {
+            if (CurrentState)
+            {
+                EnableButton = false;
+                DisableButton = true;
+            }
+            else
+            {
+                EnableButton = true;
+                DisableButton = false;
+            }
+        }
+
+        private static void SignOut()
+        {
+            var signOut = new ProcessStartInfo
+            {
+                FileName = "shutdown.exe",
+                Arguments = "/l",
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+            Process.Start(signOut);
+        }
+    }
+}
